0e05b627ecf2e55c477ffe4e0660d5dde2ae7039
AlexandrStepanov <43805840+AlexandrStepanov@users.noreply.github.com>
2021-11-01 19:04:20 +0900

    ver 1.2
    update cout and menu

--- Regular/Regular/Regular.cpp
+++ Regular/Regular/Regular.cpp
@@ -54,7 +54,7 @@ void VersionOne(string s, const int len)
         i++;
         flag = false;
     }
-    //CoutText(s,len);
+    CoutText(s,len);
 }
 void VersionTwo(string s, const int len)
 {
@@ -66,14 +66,14 @@ int main()
     unsigned int end_time;
     unsigned int search_time;
     srand(time(NULL));
-    const int N = 10000;
+    const int N = 100;
     string text;
     text=new char[N];
     int MenuParameter = 1;
     while (MenuParameter) 
     {
         gen_random(text, N);
-        //CoutText(text, N);
+        CoutText(text, N);
         Upper(text, N);
         //CoutText(text, N);
         start_time = clock();
@@ -82,18 +82,7 @@ int main()
         cout << endl;
         search_time = end_time - start_time;
         cout << "time=" << search_time / 1000.0<<endl;
-        cout << "Next?" << endl;
+        cout << "0 - exit, another num - next string" << endl;
         cin >> MenuParameter;
     }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
